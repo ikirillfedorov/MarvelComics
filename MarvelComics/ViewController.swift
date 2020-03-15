a7cef05affef5643c8e2198a054b7df555d3aa8b
Кирилл Федоров <ikirillfedorov@gmail.com>
2020-03-15 20:35:35 +0300

    Initial Commit

---
+++ MarvelComics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelComics
+//
+//  Created by Kirill Fedorov on 15.03.2020.
+//  Copyright © 2020 Kirill Fedorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
